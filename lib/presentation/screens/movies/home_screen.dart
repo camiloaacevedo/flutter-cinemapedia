f7666fd8b1d5d52d4c112e25af767fe1eb024c79
camiloaacevedo <ccuevasa@uninorte.edu.co>
2025-09-03 22:22:01 -0500

    datasource and mapper implemented

--- lib/presentation/screens/movies/home_screen.dart
+++ lib/presentation/screens/movies/home_screen.dart
@@ -1,5 +1,5 @@
-import 'package:cinemapedia/config/constants/environment.dart';
 import 'package:flutter/material.dart';
+import 'package:cinemapedia/config/constants/environment.dart';
 
 
 class HomeScreen extends StatelessWidget {
@@ -10,10 +10,8 @@ class HomeScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      body: Center(
-        child: Text( Environment.theMovieDbKey ),
-      ),
+    return const Scaffold(
+      body: Placeholder()
     );
   }
 }

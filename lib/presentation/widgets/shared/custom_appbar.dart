c3dda61805a204a06d972a9f20b5e50f04d1ac3d
camiloaacevedo <ccuevasa@uninorte.edu.co>
2025-09-15 04:53:26 -0500

    select and show movie

--- lib/presentation/widgets/shared/custom_appbar.dart
+++ lib/presentation/widgets/shared/custom_appbar.dart
@@ -1,9 +1,11 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:go_router/go_router.dart';
+
+import 'package:cinemapedia/domain/entities/movie.dart';
 
 import 'package:cinemapedia/presentation/delegates/search_movie_delegate.dart';
 import 'package:cinemapedia/presentation/providers/providers.dart';
-
 
 class CustomAppbar extends ConsumerWidget {
   const CustomAppbar({super.key});
@@ -28,19 +30,24 @@ class CustomAppbar extends ConsumerWidget {
 
               const Spacer(),
 
-              IconButton(onPressed: (){
+              IconButton(onPressed: () {
 
                 final movieRepository = ref.read( movieRepositoryProvider );
 
-
-                showSearch(
+                showSearch<Movie?>(
                   context: context, 
                   delegate: SearchMovieDelegate(
                     searchMovies: movieRepository.searchMovies
                   )
-                );
+                ).then((movie) {
+                  if ( movie == null ) return;
+                  
+                  if (context.mounted) {
+                    context.push('/movie/${ movie.id }');
+                  }
+                });
 
-              }, 
+              },
               icon: const Icon(Icons.search)
               )
             ],

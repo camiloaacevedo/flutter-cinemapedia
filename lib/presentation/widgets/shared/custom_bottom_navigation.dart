dbc470d5dcf59c64ef1312779c3833176cea9cc0
camiloaacevedo <ccuevasa@uninorte.edu.co>
2025-09-19 10:14:05 -0500

    views preparation

--- lib/presentation/widgets/shared/custom_bottom_navigation.dart
+++ lib/presentation/widgets/shared/custom_bottom_navigation.dart
@@ -19,7 +19,7 @@ class CustomBottomNavigation extends StatelessWidget {
         ),
         BottomNavigationBarItem(
           icon:  Icon( Icons.star_outline ),
-          label: 'Most Popular'
+          label: 'Favorites'
         ),
       ]
     );
